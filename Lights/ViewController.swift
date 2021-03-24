3b5bf932ce787ebf680cb48c46b158c0bbad080c
MatveenkovNikita <matveenkoff@gmail.com>
2021-03-24 14:29:56 +0300

    kek

--- Lights/ViewController.swift
+++ Lights/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        redView.layer.cornerRadius = 70
+        yellowView.layer.cornerRadius = 70
+        greenView.layer.cornerRadius = 70
+        startButton.layer.cornerRadius = 20
     }
-
-
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redView.alpha == 0.3,
+           yellowView.alpha == 0.3,
+           greenView.alpha == 0.3 {
+                redView.alpha = 1
+            return
+            }
+        if redView.alpha != 0.3,
+           yellowView.alpha == 0.3,
+           greenView.alpha == 0.3 {
+                redView.alpha = 0.3
+                yellowView.alpha = 1
+            return
+            }
+        if redView.alpha == 0.3,
+           yellowView.alpha != 0.3,
+           greenView.alpha == 0.3 {
+                yellowView.alpha = 0.3
+                greenView.alpha = 1
+            return
+            }
+        if redView.alpha == 0.3,
+           yellowView.alpha == 0.3,
+           greenView.alpha != 0.3 {
+                greenView.alpha = 0.3
+                redView.alpha = 1
+            return
+            }
+    
+        }
 }
+
 
